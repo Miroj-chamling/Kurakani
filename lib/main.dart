b931f6697ece15e943f44e43052665f8995a13e3
Miroj <zorim.rai@gmail.com>
2022-08-29 07:34:44 +0545

    first commit:

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:kurakani/screens/chat_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const ChatScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => FirebaseFirestore.instance
+            .collection('chats/HHnrKgfh4ByFB595NrLy/messages')
+            .snapshots()
+            .listen((event) {
+          for (var element in event.docs) {
+            print(element["text"]);
+          }
+        }),
+        child: Icon(Icons.message),
+      ),
+    );
+  }
+}
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Kurakani"),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => FirebaseFirestore.instance
+              .collection('chats/HHnrKgfh4ByFB595NrLy/messages')
+              .snapshots()
+              .listen((event) {
+            for (var element in event.docs) {
+              print(element["text"]);
+            }
+          }),
+          child: const Icon(Icons.message),
+        ),
+        body: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('chats/HHnrKgfh4ByFB595NrLy/messages')
+                .snapshots(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+              if (streamSnapshot.hasError) {
+                return Text(streamSnapshot.error.toString());
+              }
+              if (streamSnapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              final data = streamSnapshot.requireData;
+              return ListView.builder(
+                  itemCount: data.docs.length,
+                  itemBuilder: (context, index) {
+                    return Text(data.docs[index]["text"]);
+                  });
+            }));
+  }
+}
